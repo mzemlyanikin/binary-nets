0af0fa70f919cc44682269ba9c47d9a5c0b55611
Maxim Zemlyanikin <maxim.zemlyanikin@xperience.ai>
2019-03-01 16:23:39 +0300

    Added imagenet weights initialization

---
+++ load_weights.py
@@ -0,0 +1,53 @@
+import torch
+import torch.utils.model_zoo as model_zoo
+from urllib.parse import urlparse
+
+
+def load_weights(network, save_path, partial=False):
+    if urlparse(save_path).scheme != '':
+        pretrained_dict = model_zoo.load_url(save_path)
+    else:
+        pretrained_dict = torch.load(save_path)
+
+    if 'state_dict' in pretrained_dict:
+        pretrained_dict = pretrained_dict['state_dict']
+    try:
+        network.load_state_dict(pretrained_dict)
+        print('Pretrained network has absolutely the same layers!')
+    except:
+        model_dict = network.state_dict()
+        pretrained_dict = {k: v for k, v in pretrained_dict.items() if k in model_dict}
+        try:
+            network.load_state_dict(pretrained_dict)
+            print('Pretrained network has excessive layers; Only loading layers that are used')
+        except:
+            print('Pretrained network has fewer layers; The following are not initialized:')
+            not_initialized = set()
+            partially_initialized = set()
+            for k, v in pretrained_dict.items():
+                if v.size() == model_dict[k].size():
+                    model_dict[k] = v
+                elif partial:
+                    sizes_div = [model_dict[k].size()[i] % v.size()[i] for i in range(len(min(v.size(), model_dict[k].size())))]
+                    if not any(sizes_div):
+                        model_dict[k] = v.repeat(*[model_dict[k].size()[i] // v.size()[i] for i in range(len(min(v.size(), model_dict[k].size())))])
+                    else:
+                        min_shape = [min(v.size()[i], model_dict[k].size()[i]) for i in range(len(min(v.size(), model_dict[k].size())))]
+                        if len(model_dict[k].size()) in [2, 4]: # fc and conv layers
+                            model_dict[k][:min_shape[0], :min_shape[1], ...] = \
+                                v[:min_shape[0], :min_shape[1], ...]
+                        elif len(model_dict[k].size()) == 1:
+                            model_dict[k][:min_shape[0]] = v[:min_shape[0]]
+                        else:
+                            print('{} has size: '.format(k, model_dict[k].size()))
+
+            for k, v in model_dict.items():
+                if k not in pretrained_dict or (not partial and v.size() != pretrained_dict[k].size()):
+                    not_initialized.add(k)
+                elif partial and v.size() != pretrained_dict[k].size():
+                    partially_initialized.add(k)
+            print(sorted(not_initialized))
+            if partial:
+                print('Partially initialized:')
+                print(sorted(partially_initialized))
+            network.load_state_dict(model_dict)
--- models.py
+++ models.py
@@ -1,6 +1,6 @@
 from basic_blocks import BinaryBasicBlock, ParallelBinaryBasicBlockWithSqueeze, ParallelBinaryBasicBlockNoSqueeze, ParallelBinaryBasicBlockWithFusionGate
 from resnet import ResNet, ReIdResNet
-
+from load_weights import load_weights
 
 model_urls = {
     'resnet18': 'https://download.pytorch.org/models/resnet18-5c106cde.pth',
@@ -11,42 +11,58 @@ model_urls = {
 }
 
 
-def binary_resnet18(**kwargs): # paper, page 5, fig. 1(b)
+def binary_resnet18(pretrained='imagenet', **kwargs): # paper, page 5, fig. 1(b)
     model = ResNet(BinaryBasicBlock, [2, 2, 2, 2], **kwargs)
+    if pretrained == 'imagenet':
+        load_weights(model, model_urls['resnet18'], partial=True)
     return model
 
 
-def parallel_resnet18_no_squeeze(**kwargs): # paper, page 5, fig. 2 (c)
+def parallel_resnet18_no_squeeze(pretrained='imagenet', **kwargs): # paper, page 5, fig. 2 (c)
     model = ResNet(ParallelBinaryBasicBlockNoSqueeze, [2, 2, 2, 2], **kwargs)
+    if pretrained == 'imagenet':
+        load_weights(model, model_urls['resnet18'], partial=True)
     return model
 
 
-def parallel_resnet18_with_squeeze(**kwargs): # paper, page 5, fig. 2 (b)
+def parallel_resnet18_with_squeeze(pretrained='imagenet', **kwargs): # paper, page 5, fig. 2 (b)
     model = ResNet(ParallelBinaryBasicBlockWithSqueeze, [2, 2, 2, 2], **kwargs)
+    if pretrained == 'imagenet':
+        load_weights(model, model_urls['resnet18'], partial=True)
     return model
 
 
-def parallel_resnet18_with_fusion_gate(**kwargs): # paper, page 6, fig. 3
+def parallel_resnet18_with_fusion_gate(pretrained='imagenet', **kwargs): # paper, page 6, fig. 3
     model = ResNet(ParallelBinaryBasicBlockWithFusionGate, [2, 2, 2, 2], **kwargs)
+    if pretrained == 'imagenet':
+        load_weights(model, model_urls['resnet18'], partial=True)
     return model
 
 
-def reid_binary_resnet18(loss, num_classes, **kwargs): # paper, page 5, fig. 1(b)
+def reid_binary_resnet18(loss, num_classes, pretrained='imagenet', **kwargs): # paper, page 5, fig. 1(b)
     model = ReIdResNet(loss=loss, num_classes=num_classes, block=BinaryBasicBlock, layers=[2, 2, 2, 2], **kwargs)
+    if pretrained == 'imagenet':
+        load_weights(model, model_urls['resnet18'], partial=True)
     return model
 
 
-def reid_parallel_resnet18_no_squeeze(loss, num_classes, **kwargs): # paper, page 5, fig. 2 (c)
+def reid_parallel_resnet18_no_squeeze(loss, num_classes, pretrained='imagenet', **kwargs): # paper, page 5, fig. 2 (c)
     model = ReIdResNet(loss=loss, num_classes=num_classes, block=ParallelBinaryBasicBlockNoSqueeze, layers=[2, 2, 2, 2], **kwargs)
+    if pretrained == 'imagenet':
+        load_weights(model, model_urls['resnet18'], partial=True)
     return model
 
 
-def reid_parallel_resnet18_with_squeeze(loss, num_classes, **kwargs): # paper, page 5, fig. 2 (b)
+def reid_parallel_resnet18_with_squeeze(loss, num_classes, pretrained='imagenet', **kwargs): # paper, page 5, fig. 2 (b)
     model = ReIdResNet(loss=loss, num_classes=num_classes, block=ParallelBinaryBasicBlockWithSqueeze, layers=[2, 2, 2, 2], **kwargs)
+    if pretrained == 'imagenet':
+        load_weights(model, model_urls['resnet18'], partial=True)
     return model
 
 
-def reid_parallel_resnet18_with_fusion_gate(loss, num_classes, **kwargs): # paper, page 6, fig. 3
-    model = ReIdResNet(loss=loss, num_classes=num_classes, block=ParallelBinaryBasicBlockWithFusionGate, layers=[2, 2, 2, 2], parallel=2, **kwargs)
+def reid_parallel_resnet18_with_fusion_gate(loss, num_classes, pretrained='imagenet', **kwargs): # paper, page 6, fig. 3
+    model = ReIdResNet(loss=loss, num_classes=num_classes, block=ParallelBinaryBasicBlockWithFusionGate, layers=[2, 2, 2, 2], **kwargs)
+    if pretrained == 'imagenet':
+        load_weights(model, model_urls['resnet18'], partial=True)
     return model
 
